2f9e3de09b1c71bfa70d7b497bf6ae6240550b9b
ShrutiTripathi <shruti.tripathi@jktech.com>
2014-05-06 08:00:44 +0100

    ThreeStoriesMerged&CommittedForBeneficiaryDetailsScreen

--- src/com/jkt/donateme/client/DonateMe_GWT.java
+++ src/com/jkt/donateme/client/DonateMe_GWT.java
@@ -5,16 +5,8 @@ import net.customware.gwt.presenter.client.EventBus;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.jkt.donateme.client.presenter.BeneficiaryDetailsPresenter;
 import com.jkt.donateme.client.presenter.SignUpPresenter;
-import com.jkt.donateme.client.view.BeneficiaryDetailsView;
 import com.jkt.donateme.client.view.SignUpView;
-//import com.jkt.donateme.client.presenter.BeneficiaryDetailsPresenter;
-//import com.jkt.donateme.client.presenter.ProfileDetailPresenter;
-//import com.jkt.donateme.client.presenter.SignUpPresenter;
-//import com.jkt.donateme.client.view.BeneficiaryDetailsView;
-//import com.jkt.donateme.client.view.ProfileDetailView;
-//import com.jkt.donateme.client.view.SignUpView;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -29,13 +21,9 @@ public class DonateMe_GWT implements EntryPoint {
 	public void onModuleLoad() {
 
 		EventBus eventBus = new DefaultEventBus();
-	//	ProfileDetailView display = new ProfileDetailView();
-	//	ProfileDetailPresenter presenter = new ProfileDetailPresenter(display, eventBus);
 
-		//SignUpView display = new SignUpView();
-		//SignUpPresenter presenter = new SignUpPresenter(display, eventBus);
-		BeneficiaryDetailsView display = new BeneficiaryDetailsView();
-		BeneficiaryDetailsPresenter presenter = new BeneficiaryDetailsPresenter(display, eventBus);
+		SignUpView display = new SignUpView();
+		SignUpPresenter presenter = new SignUpPresenter(display, eventBus);
 		presenter.bind();
 
 		RootPanel.get("top").add(presenter.getDisplay().asWidget());
